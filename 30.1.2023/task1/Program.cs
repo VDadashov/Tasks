73902f144031f820519b8d179b570963ba250c1b
Vusal <vusald79@gmail.com>
2023-01-27 18:10:53 +0400

    30.1.2023

---
+++ 30.1.2023/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verilmiş ədədin rəqəmləri cəmini tapan proqram
+            // N = 234
+
+            Console.WriteLine("Ededi daxil edin");
+            string input = Console.ReadLine();
+            int N  = Convert.ToInt32(input);
+
+            int sum = 0;
+
+            while (N > 0)
+            {
+                int index = N % 10;
+                sum += index;
+                N /= 10;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
