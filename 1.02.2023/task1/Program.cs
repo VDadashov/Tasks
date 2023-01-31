d32ba6605bbe8f512f67149fd37aeaceaeb6c56a
Vusal <vusald79@gmail.com>
2023-02-01 01:09:57 +0400

    modul tasks

---
+++ 1.02.2023/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveStarSpace("  Hello World"));
+        }
+
+        // Verilmiş yazının əvvəlindəki boşluqları silib qaytaran metod
+
+        // "  Hello World"
+
+ 
+
+        static string RemoveStarSpace(string text)
+        {
+            string newStr = "";
+            int index = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                
+                if (text[i] != ' ')
+                {
+                    index = i;
+                    break;
+                }
+
+            }
+
+            for (int j = index; j < text.Length; j++)
+            {
+                newStr += text[j];
+            }
+            return newStr;
+        }
+    }
+}
---
+++ 1.02.2023/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, -5, 45, 3, -11, 24 };
+
+            int[] newArr = MakePositive(numbers);
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (newArr[i] != 0)
+                {
+                    Console.WriteLine(newArr[i]);
+                }
+                
+            }
+
+            
+
+        }
+
+        // Verilmiş ədədlər siyahısından yalnız müsbət ədədlərindən ibarət
+        // yeni bir array düzəldib qaytaran metod
+
+        static int[] MakePositive(int[] numbers)
+        {
+            int[] newArr = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]>0)
+                {
+                    newArr[i] = numbers[i];
+                }
+            }
+
+            return newArr;
+
+            
+
+        }
+
+
+
+    }
+}
