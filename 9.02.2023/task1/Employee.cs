e83a3d06134f3a0f45aa6b8d410cec669bc40194
Vusal <vusald79@gmail.com>
2023-02-08 23:50:38 +0400

    9.02.2023

---
+++ 9.02.2023/task1/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    internal class Employee
+    {
+        public string FullName;
+        protected int _salary;
+
+        public virtual int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value > 300)
+                {
+                    _salary = value;
+                    
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ 9.02.2023/task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee();
+
+            //employee.Salary = 10;
+            //employee.Salary = 450;
+
+            //Console.WriteLine(employee.Salary);
+
+            Console.Write("Engineer count: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Engineer[] engineers = new Engineer[count];
+
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"\n{i+1},Engineer'i daxil edin");
+
+                Console.WriteLine("\nFullName'i daxil edin:");
+                string fuLllName = Console.ReadLine();
+
+                Console.WriteLine("Salary'i daxil edin:");
+                trySalary: int salary = Convert.ToInt32(Console.ReadLine());
+
+                if (salary < 1000)
+                {
+                    Console.WriteLine("salary 1000'den aşağı ola bilmez");
+                    goto trySalary;
+                }
+
+                Engineer engineer = new Engineer() { FullName = fuLllName, Salary = salary };
+
+
+                Console.WriteLine("-----------------");
+
+                
+                engineers[i] = engineer;
+            }
+
+            double sum = 0;
+
+            for (int i = 0; i < engineers.Length; i++)
+            {
+                sum += engineers[i].Salary;
+            }
+
+            var result = sum / engineers.Length;
+
+
+            Console.WriteLine($"Maash ortalamasi: {result}");
+            
+        }
+    }
+}
