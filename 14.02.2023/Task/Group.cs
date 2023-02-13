2ae0d65f04df249968f08437c5d59a6d7fce1dab
Vusal <vusald79@gmail.com>
2023-02-13 22:10:34 +0400

    14.02.2023

---
+++ 14.02.2023/Task/Group.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    internal class Group
+    {
+        string _no;
+        double _avgPoint;
+
+        public string No
+        {
+            get { return _no; }
+            set { _no = value; }
+        }
+        public double AvgPoint
+        {
+            get
+            {
+                return _avgPoint;
+            }
+            set 
+            {
+                if (value>=0 && value <= 100)
+                {
+                    _avgPoint = value;
+                } 
+            }
+        }
+
+    }
+}
---
+++ 14.02.2023/Task/ICourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    internal interface ICourse
+    {
+        Group[] AddGroup(Group group);
+        Group GetGroupByNo(string no);
+        Group[] GetGroupsByPointRange(int minPoint,int maxPoint);
+        Group[] Search(string str);
+
+    }
+}
