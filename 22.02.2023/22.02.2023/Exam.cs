5c8056a2ea8bc2209f4b6f7e15c9ebdffd1d2916
Vusal <vusald79@gmail.com>
2023-02-22 07:17:56 +0400

    22.02.2023

---
+++ 22.02.2023/22.02.2023/Exam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _23._02._2023
+{
+    internal class Exam
+    {
+        public string Subject { get; set; }
+        public byte Point { get; set; }
+        public DateTime Startat { get; set; }
+        public DateTime Endat { get; set; }
+    }
+}
---
+++ 22.02.2023/22.02.2023/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _23._02._2023
+{
+    internal class Student
+    {
+        public string FullName;
+        public string GroupNo;
+        public List<Exam> Exams = new List<Exam>();
+
+
+
+        public List<Exam> GetExamsByPointRange(int minPoint, int maxPoint)
+        {
+            List<Exam> wantedExams = new List<Exam>();
+
+            foreach (Exam item in Exams)
+            {
+                if (item.Point >= minPoint && item.Point <= maxPoint)
+                {
+                    wantedExams.Add(item);
+                }
+            }
+            return wantedExams;
+        }
+
+        public List<Exam> GetExamsByDateRange(DateTime fromDate, DateTime toDate)
+        {
+            List<Exam> wantedExams = new List<Exam>();
+
+            foreach (var item in Exams)
+            {
+                if (item.Startat >= fromDate && item.Startat <= toDate)
+                {
+                    wantedExams.Add(item);
+                }
+            }
+            return wantedExams;
+        }
+
+        public int GetAvgPoint()
+        {
+            int sum = 0;
+            foreach (var item in Exams)
+            {
+                sum += item.Point;
+            }
+            return sum / Exams.Count;
+
+
+        }
+    }
+}
